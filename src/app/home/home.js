86c1106c3213f571fe679ddaa108069144f0d01b
lucianlature <lucian.lature@gmail.com>
2014-06-14 19:10:24 +0100

    Implemented bid-time-left directive.

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -12,10 +12,42 @@
  * The dependencies block here is also where component dependencies should be
  * specified, as shown below.
  */
-angular.module( 'ngBoilerplate.home', [
+angular
+
+.module( 'ngBoilerplate.home', [
   'ui.router',
   'plusOne'
 ])
+
+// Register the 'bidTimeLeft' directive factory method.
+// We inject $interval and dateFilter service since the factory method is DI.
+.directive('bidTimeLeft', function($interval, dateFilter) {
+  // return the directive link function. (compile function not needed)
+  return function(scope, element, attrs) {
+
+    var format,  // date format
+        stopTime; // so that we can cancel the time updates
+
+    // used to update the UI
+    function updateTime() {
+      element.text(dateFilter(new Date(), format));
+    }
+
+    // watch the expression, and update the UI on change.
+    scope.$watch(attrs.bidTimeLeft, function(value) {
+      format = value;
+      updateTime();
+    });
+
+    stopTime = $interval(updateTime, 1000);
+
+    // listen on DOM destroy (removal) event, and cancel the next UI update
+    // to prevent updating time ofter the DOM element was removed.
+    element.on('$destroy', function() {
+      $interval.cancel(stopTime);
+    });
+  };
+})
 
 /**
  * Each section or module of the site can also have its own routes. AngularJS
@@ -38,7 +70,10 @@ angular.module( 'ngBoilerplate.home', [
 /**
  * And of course we define a controller for our route.
  */
-.controller( 'HomeCtrl', function HomeController( $scope ) {
+.controller( 'HomeCtrl', function HomeController( $scope, $interval ) {
+
+  $scope.format = 'M/d/yy h:mm:ss a';
+
   $scope.tiles = [{
     id: 'b1',
     title: 'Bid #1',
@@ -58,7 +93,30 @@ angular.module( 'ngBoilerplate.home', [
     isVisible: true,
     image: 'assets/images/bid3.jpg'
   }];
-})
+  /*
+  $sc = items.map(function(item){
+          item.timer = Math.floor((item.expires - time)/1000);
+        });
+  */
+});
 
-;
+function pretty_time_string ( num ) {
+  return ( num < 10 ? '0' : '' ) + num;
+}
 
+function timerFormat ( total_seconds ) {
+
+  var hours = Math.floor(total_seconds / 3600);
+      total_seconds = total_seconds % 3600,
+      minutes = Math.floor(total_seconds / 60),
+      total_seconds = total_seconds % 60,
+      seconds = Math.floor(total_seconds);
+
+  // Pad the minutes and seconds with leading zeros, if required
+  hours = pretty_time_string(hours);
+  minutes = pretty_time_string(minutes);
+  seconds = pretty_time_string(seconds);
+
+  // Compose the string for display
+  return hours + ":" + minutes + ":" + seconds;
+}
