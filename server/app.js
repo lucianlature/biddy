5fafd0a814f85ae248e159dc559a65bae92b05c3
lucianlature <lucian.lature@gmail.com>
2014-06-27 20:31:50 +0100

    Serve files from express and embrace socket.io.

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@
  */
 
 var express = require('express'),
-    routes = require('./routes'),
-    socket = require('./routes/socket.js');
+    socket = require('./socket.js');
 
 var app = module.exports = express.createServer();
 
@@ -13,14 +12,14 @@ var io = require('socket.io').listen(app);
 
 // Configuration
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  // app.set('views', __dirname + '/views');
+  // app.set('view engine', 'jade');
   app.set('view options', {
     layout: false
   });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/../'));
   app.use(app.router);
 });
 
@@ -34,18 +33,16 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
-app.get('/partials/:name', routes.partials);
+// app.get('/', routes.index);
+// app.get('/partials/:name', routes.partials);
 
 // redirect all others to the index (HTML5 history)
-app.get('*', routes.index);
+// app.get('*', routes.index);
 
 // Socket.io Communication
-
 io.sockets.on('connection', socket);
 
 // Start server
-
-app.listen(3000, function(){
+app.listen(8080, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
